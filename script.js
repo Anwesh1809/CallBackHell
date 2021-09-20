7976a2cafcde52a04d0c8c219c539ebcbd2e54ec
Anwesh1809 <87658220+Anwesh1809@users.noreply.github.com>
2021-09-20 13:34:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+setTimeout(() => {
+  const heading = document.querySelector("h1");
+  heading.innerText = "10";
+  setTimeout(() => {
+    const heading = document.querySelector("h1");
+    heading.innerText = "9";
+    setTimeout(() => {
+      const heading = document.querySelector("h1");
+      heading.innerText = "8";
+      setTimeout(() => {
+        const heading = document.querySelector("h1");
+        heading.innerText = "7";
+        setTimeout(() => {
+          const heading = document.querySelector("h1");
+          heading.innerText = "6";
+          setTimeout(() => {
+            const heading = document.querySelector("h1");
+            heading.innerText = "5";
+            setTimeout(() => {
+              const heading = document.querySelector("h1");
+              heading.innerText = "4";
+              setTimeout(() => {
+                const heading = document.querySelector("h1");
+                heading.innerText = "3";
+                setTimeout(() => {
+                  const heading = document.querySelector("h1");
+                  heading.innerText = "2";
+                  setTimeout(() => {
+                    const heading = document.querySelector("h1");
+                    heading.innerText = "1";
+                    setTimeout(() => {
+                      const heading = document.querySelector("h1");
+                      heading.innerText = "Are you ready!!!";
+                      setTimeout(() => {
+                        const heading = document.querySelector("h1");
+                        heading.innerText = "Let's go for it!!!";
+                        setTimeout(() => {
+                          const heading = document.querySelector("h1");
+                          heading.innerText = "Happy New Year 2022!!!";
+                        }, 1000);
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
